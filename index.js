95a1bc68cfbfc9c1a1ed02628118e0c170fde45d
jackgeek <Jack.Allan@fenergo.com>
2018-12-23 03:05:04 +0000

    fix: Fix bug where an initialInstanceState of type function always gets called every render
    closes #1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { useState, useMemo } from "react";
 
 export default function useInstance(initialInstanceState) {
-  const [{ instance }, updateInstance] = useState({
+  const [{ instance }, updateInstance] = useState(() => ({
     instance:
       typeof initialInstanceState === "function"
         ? initialInstanceState()
         : initialInstanceState
-  });
+  }));
   return useMemo(() => [
     instance,
     function update() {
